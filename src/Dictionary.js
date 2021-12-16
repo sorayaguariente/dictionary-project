8bd1fa372723c91031e3f81ee7db0077497fe4a6
Soraya Guariente <87147989+sorayaguariente@users.noreply.github.com>
2021-12-16 17:26:13 +0000

    Added new components to show results of the words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
@@ -11,12 +14,12 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   // documentation: https://dictionaryapi.dev
 
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
   axios.get(apiUrl).then(handleResponse);
 
   return (
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} autoFocus={true} />
       </form>{" "}
+      <Results results={results} />
     </div>
   );
 }
