125ebf357ac6e631ebcee5c5e4773b2e71ee0c40
Soraya Guariente <87147989+sorayaguariente@users.noreply.github.com>
2021-12-20 15:56:24 +0000

    Added synonyms content

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -9,10 +10,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
-              <em>{definition.exemple}</em>
+              <strong>Exemple:</strong>
+              <em> {definition.exemple}</em>
             </p>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
