387eb3dad043307910e9083face4c5ef10e79dfb
Soraya Guariente <87147989+sorayaguariente@users.noreply.github.com>
2021-12-29 11:45:11 +0000

    instaled fonts-types

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./fonts/Carter_One/CarterOne-Regular.ttf";
+import "./fonts/Courier_Prime/CourierPrime-Bold.ttf";
+import "./fonts/Courier_Prime/CourierPrime-BoldItalic.ttf";
+import "./fonts/Courier_Prime/CourierPrime-Italic.ttf";
+import "./fonts/Courier_Prime/CourierPrime-Regular.ttf";
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
