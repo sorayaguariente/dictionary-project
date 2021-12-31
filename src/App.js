d1616ece37aa91b551ecbedcb1c356b9cbebc271
Soraya Guariente <87147989+sorayaguariente@users.noreply.github.com>
2021-12-31 12:44:44 +0000

    Added github link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,27 @@ export default function App() {
       </header>
       <main>
         <Dictionary defaultKeyWord="sunset" />
+        <div className="App-github">
+          This project was coded by
+          <a
+            href="https://www.linkedin.com/in/soraya-guariente-04b1a3166/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Soraya Guariente{" "}
+          </a>{" "}
+          and is open-sorced on{" "}
+          <a
+            href="https://github.com/sorayaguariente/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
       </main>
+
       <footer className="App-footer">Soraya Guariente 2021</footer>
     </div>
   );
